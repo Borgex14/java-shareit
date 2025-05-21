fd6996146c6f26a4cd08a039bd040c4ee96da8b7
Ageu <Borzhesh@yandex.ru>
2025-05-21 23:27:41 +0300

    добавил поддержку preview-фич при компиляции и запуске

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -3,11 +3,10 @@ package ru.practicum.shareit;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ShareItTests {
+@SpringBootTest(classes = ShareItApp.class)
+public class ShareItTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
