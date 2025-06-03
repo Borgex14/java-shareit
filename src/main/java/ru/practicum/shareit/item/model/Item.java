6f1928ae8f6a967dac9c68de6d0663278d91d697
Ageu <Borzhesh@yandex.ru>
2025-06-03 21:04:51 +0300

    Добавил соответствующие аннотации для сущностей.

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,18 +1,35 @@
 package ru.practicum.shareit.item.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import ru.practicum.shareit.user.model.User;
 
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
+@Entity
+@Table(name = "items")
 public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name", nullable = false, length = 255)
     private String name;
+
+    @Column(name = "description")
     private String description;
+
+    @Column(name = "available", nullable = false)
     private Boolean available;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id", nullable = false)
     private User owner;
+
+    @Column(name = "request", length = 512)
     private String request;
 }
--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -1,14 +1,24 @@
 package ru.practicum.shareit.user.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name", nullable = false)
     private String name;
+
+    @Column(name = "email", nullable = false, unique = true, length = 512)
     private String email;
 }
