cf889fb01a4fcb0c488310c2c52a40ee6293c918
Ageu <Borzhesh@yandex.ru>
2025-06-09 17:29:18 +0300

    Создал стиль в класс

--- src/main/java/ru/practicum/shareit/booking/service/BookingService.java
+++ src/main/java/ru/practicum/shareit/booking/service/BookingService.java
@@ -8,8 +8,12 @@ import java.util.List;
 
 public interface BookingService {
     BookingResponseDto createBooking(Long userId, BookingRequestDto bookingRequestDto);
+
     BookingResponseDto approveBooking(Long userId, Long bookingId, Boolean approved);
+
     BookingResponseDto getBooking(Long userId, Long bookingId);
+
     List<BookingResponseDto> getUserBookings(Long userId, BookingFilterState state, Integer from, Integer size);
+
     List<BookingResponseDto> getOwnerBookings(Long userId, BookingFilterState state, Integer from, Integer size);
 }
