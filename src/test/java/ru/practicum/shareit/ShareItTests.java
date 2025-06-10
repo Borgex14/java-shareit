324628c5b05a0ddd423098778eea0b66752fd702
Ageu <Borzhesh@yandex.ru>
2025-06-10 12:59:12 +0300

    Доработал настройку кода для теста

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -2,9 +2,11 @@ package ru.practicum.shareit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest(classes = ShareItApp.class)
-public class ShareItTests {
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+class ShareItTests {
 
 	@Test
 	void contextLoads() {
