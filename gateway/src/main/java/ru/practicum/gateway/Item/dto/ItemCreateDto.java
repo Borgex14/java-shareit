3d99e82645734d84c419de6760fb34f0f36ed761
Ageu <Borzhesh@yandex.ru>
2025-07-03 23:51:02 +0300

    Удалил неиспользуемые импорты

--- gateway/src/main/java/ru/practicum/gateway/Item/dto/ItemCreateDto.java
+++ gateway/src/main/java/ru/practicum/gateway/Item/dto/ItemCreateDto.java
@@ -3,9 +3,6 @@ package ru.practicum.gateway.Item.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-import ru.practicum.gateway.request.ItemRequest;
-import ru.practicum.gateway.user.User;
-import ru.practicum.gateway.user.dto.UserDto;
 
 @Data
 public class ItemCreateDto {
